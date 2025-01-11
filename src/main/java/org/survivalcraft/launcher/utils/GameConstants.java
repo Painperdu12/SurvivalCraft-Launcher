5eeea274c1c0bb07dbf564ba1a1da2a78469ee8a
Painperdu12 <maxden2107@gmail.com>
2025-01-11 12:59:22 +0100

    Mise à Jour de l'URL de téléchargement des mods

--- src/main/java/org/survivalcraft/launcher/utils/GameConstants.java
+++ src/main/java/org/survivalcraft/launcher/utils/GameConstants.java
@@ -4,5 +4,5 @@ public class GameConstants {
 
     public static final String VERSION = "1.19.2";
     public static final String FORGE_VERSION = "1.19.2-43.0.11";
-    public static final String MOD_LIST_URL = "https://www.survivalcraft.org/files/mods.json";
+    public static final String MOD_LIST_URL = "http://survivq.cluster029.hosting.ovh.net/files/mods.json";
 }
